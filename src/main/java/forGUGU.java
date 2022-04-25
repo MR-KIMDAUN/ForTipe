89c0664e0e96813aa9df307ebe64c1d32bff6d54
MR-KIMDAUN <people5474@naver.com>
2022-04-25 15:59:49 +0900

    20220425

--- src/main/java/forGUGU.java
+++ src/main/java/forGUGU.java
@@ -2,9 +2,13 @@ public class forGUGU {
     public static void main(String[] args) {
 
         for(int a = 2; a < 10; a++){
-            for(int num = 1; num < 10; num++) {
-                System.out.printf("%d * %d = %d \n", a, num, a * num);
+            if ((a == 3) || (a == 5)) {
+                System.out.printf("<%d 단>\n",a);
             }
+                for (int num = 1; num < 10; num++) {
+                    System.out.printf("%d * %d = %d \n", a, num, a * num);
+                }
+
             System.out.println();
         }
     }
